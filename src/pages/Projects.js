8165d8b7e176c55b01946d7c10df3b79c8087b5c
Essence Fuller <essencefuller@Essences-MacBook-Pro.local>
2022-06-14 14:28:05 -0700

    frontend complete

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,38 @@
+import {useState, useEffect} from "react";
+
 function Projects(props) {
-    return <h1>Projects</h1>
+
+    const [projects, setProjects] = useState(null)
+
+    const getProjectsData = async() => {
+        const response = await fetch(props.URL + "projects")
+        const data = await response.json();
+        setProjects(data)
+    }
+
+    useEffect(()=> {
+        getProjectsData();
+    }, [])
+
+
+    const loaded= () => {
+    return Projects.map((project) => (
+        <div>
+            <h1>{project.name}</h1>
+            <img src={project.image} alt= {project.name}/>
+
+            <a href={project.git}>
+                <button>Github</button>
+            </a>
+
+            <a href={project.live}>
+                <button>Live Site</button>
+            </a>
+        </div>
+    )) 
+}
+
+    return projects ? loaded() : <h1>Loading...</h1>
 }
 
 export default Projects;
